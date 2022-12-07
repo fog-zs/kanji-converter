7dcbda892477d2de29fa8999009532044436f172
fog <38463346+fog-zs@users.noreply.github.com>
2022-12-08 01:07:05 +0900

    Fix: a bug that characters disappear when simplified Chinese
    Add: Pinyin and Zhuyin
    modified:   src/components/KanjiConverter.jsx
    modified:   src/kanji.json

--- src/components/KanjiConverter.jsx
+++ src/components/KanjiConverter.jsx
@@ -1,45 +1,46 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import TextField from '@mui/material/TextField';
 import Grid from '@mui/material/Grid';
 import dataJson from "../kanji.json";
+import pinyin from "pinyin";
+import zhuyin from "zhuyin";
 
 const KanjiConverter = ()=> {
     const [strJA, setJA] = useState('');
     const [strTW, setTW] = useState('');
     const [strCN, setCN] = useState('');    
+    const [strPinyin, setPinyin] = useState('');    
+    const [strZhuyin, setZhuyin] = useState('');    
     
     const countryCode = ["ja", "zh-TW", "zh-CN"];
 
     // 文字列をまとめて置換するプログラム
     const strTranslate = (type, sourceText) =>{
         const table = dataJson
-        let text = sourceText.slice()
+        let text = sourceText
         
         // 全文字のループ
         for (let [index, value] of table[type].entries())
         {                        
             countryCode.forEach ((code)=>{
                 // 国のコードの置換元と置換対象が一緒の場合スキップ
-                if (code === type) return;
-
-                let target = table[code][index]
-    
-                // 空白の場合はスキップする
-                if (target === "") {
+                if (code === type) {
                     return;
                 }
 
+                const source = table[code][index]
+    
                 // 空白の場合はスキップする
-                if (value === ""){
+                if (source === "" || value === "") {
                     return;
                 }
 
                 // 文字が同じときスキップする
-                if (target === value) {
+                if (source === value) {
                     return;
                 }
 
-                text = text.replaceAll(target, value)
+                text = text.replaceAll(source, value)
             })
         }
         return text
@@ -49,11 +50,34 @@ const KanjiConverter = ()=> {
         setJA(strTranslate(countryCode[0], event.target.value));
         setTW(strTranslate(countryCode[1], event.target.value));
         setCN(strTranslate(countryCode[2], event.target.value));
+        getPinyin(event.target.value);
     };    
+    
+    const getPinyin = (text) =>{
+        if (text === "") {
+            setPinyin("");
+            return;
+        }
+        const txt = pinyin(text);
+        setPinyin(txt.join(','));        
+    }
+    
+    useEffect(() => {
+        getZhuyin(strPinyin);
+    }, [strPinyin])
+
+    const getZhuyin = (text) =>{
+        if (text === "") {
+            setZhuyin("");
+            return;
+        }
+        const txt = zhuyin(text);
+        setZhuyin(txt);        
+    }
 
     return (
       <div>
-        <Grid container direction="column" spacing={4}>
+        <Grid container direction="column" spacing={3}>
             <Grid item>
                 <TextField
                     id="outlined-multiline-static"
@@ -86,6 +110,30 @@ const KanjiConverter = ()=> {
                     rows={5}
                     value={strCN}
                     onChange={onChangeText}
+                    sx={{ width: '50ch' }}
+                />
+            </Grid>
+            <Grid item>
+                
+                <TextField
+                    disabled
+                    id="outlined-multiline-static"
+                    label="拼音"
+                    multiline
+                    rows={5}
+                    value={strPinyin}
+                    sx={{ width: '50ch' }}
+                />
+            </Grid>
+            <Grid item>
+                
+                <TextField
+                    disabled
+                    id="outlined-multiline-static"
+                    label="注音"
+                    multiline
+                    rows={5}
+                    value={strZhuyin}
                     sx={{ width: '50ch' }}
                 />
             </Grid>
